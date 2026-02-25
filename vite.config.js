5df1085ec262d6f8d12759a9d01b0bf11652f1aa
IdikaElaida <idika1996@gmail.com>
2026-02-26 00:28:03 +0100

    setting up vite, added PWA and tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,5 +11,7 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
+    VitePWA(),
+    tailwindcss(), // <--- Add this here
   ],
 })
